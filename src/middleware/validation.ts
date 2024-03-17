b2c71bb971106483a982dc752fc3f5b7b7ac1c0b
ShaanKapoor10 <shaan2003kapoor@gmail.com>
2024-03-17 16:31:15 +0530

    backend for deployment uptil user profile

---
+++ src/middleware/validation.ts
@@ -0,0 +1,22 @@
+
+import { body, validationResult } from "express-validator";
+import { Request , Response , NextFunction} from "express";
+
+const handleValidationErrors = async(req : Request,res : Response, next : NextFunction) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty())
+    {
+        console.log(errors);
+        console.log("BHAI MATTER");
+        return res.status(400).json({ errors : errors.array() });
+    }
+    next();
+};
+
+export const validateMyUserRequest = [
+     body("name").isString().notEmpty().withMessage("Name must be a string"),
+     body("addressLine1").isString().notEmpty().withMessage("AddressLine1 must be a string"),
+     body("city").isString().notEmpty().withMessage("City must be a string"),
+     body("country").isString().notEmpty().withMessage("Country must be a string"),
+     handleValidationErrors,
+];
